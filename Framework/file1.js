16192e501c13fc7d4b037438b6a9a42d00dd6580
중화이 <ljh15952@gmail.com>
2022-12-04 11:49:10 +0000

    키보드 동시 입력 이벤트

--- Framework/file1.js
+++ Framework/file1.js
@@ -6,4 +6,20 @@ cfx = canvas.getContext("2d");
 function print(){
 	console.log("q123weqwe");
 }
+// a 65
+// s 83
+// d 68
+// f 87
+var keys = [];
+var x = 0;
+var y = 0;
+window.onkeydown = (e) =>{
+	console.log(e.keyCode);
+	keys[e.keyCode] = true;
+	
+	
+}
 
+window.onkeyup = (e) => {
+	keys[e.keyCode] = false;
+}
--- Framework/file2.js
+++ Framework/file2.js
@@ -6,11 +6,26 @@ setTimeout(print, 5000);
 var counter = 0;
 
 function Mainloop(){
+	cfx.clearRect(0,0,500,500);
 	counter++;
 	if(counter >= 5){
-		clearInterval(mainLoop)
+		//clearInterval(mainLoop)
 	}
-	console.log("this is main loop");
+	if(keys[65]){
+		x--;
+	}
+	if(keys[83]){
+		y++;
+	}
+	if(keys[68]){
+		x++;
+	}
+	if(keys[87]){
+		y--;
+	}
+	//console.log("this is main loop");
+	console.log(x + " " + y);
+	cfx.fillRect(x,y,10,10);
 }
 
-let mainLoop = setInterval(Mainloop, 1000);
+let mainLoop = setInterval(Mainloop, 10);
