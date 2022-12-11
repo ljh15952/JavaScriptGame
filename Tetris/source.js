7c25724847cc5f009f84cd2976cc612d96d8f5d2
중화이 <ljh15952@gmail.com>
2022-12-11 11:43:34 +0000

    20:43

--- Tetris/source.js
+++ Tetris/source.js
@@ -1,4 +1,17 @@
 const data = {score: 0, oldCoords: null, newCoords: null,
 			   pos: {x: 3, y: -2}, over:false};
 
-console.log("qwe");
+const renderWell = () => {
+	document.querySelector('pre').textContent = `${data.score}\n`;
+	well.foeEach(r => {document.querySelector('pre').textContent += `${r.join('')}\n`});
+};
+
+//■
+
+let well = Array(20).fill(0).map(() => Array(10).fill('□'))
+
+
+
+console.log(data);
+console.log(renderWell);
+console.log(well);
