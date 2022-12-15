aaafdaf63720af0121371f4a5829025483f46146
중화이 <ljh15952@gmail.com>
2022-12-15 11:44:23 +0000

    20:44

--- Tetris/source.js
+++ Tetris/source.js
@@ -27,14 +27,31 @@ console.log(data);
 console.log(renderWell);
 console.log(well);
 
+const test1 = () =>{
+	console.log("1111111111");
+	return true;
+}
+
+const test2 = () => {
+	console.log("2222222222");
+}
 
 let before = Date.now();
 const freeFall = () => {
 	const now = Date.now();;
 	if(now - before >= 500){
 		before = now;
-		console.log("HI?");
+		console.log("UPDATE THIS");
 	}
+	
+	test1() && test2(); // <- test1() 함수가 return true라면 뒤에 있는 test2() 함수도 실행
+	/*
+	if(test1()){
+		test2(); 랑 같은 코드 인데 한 줄로 줄였네요?
+	}
+	
+	*/
+	
 	requestAnimationFrame(freeFall);
 }
 
