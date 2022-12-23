361e25486ff572caa94ec5070df52d56acf47ade
중화이 <ljh15952@gmail.com>
2022-12-23 14:17:05 +0000

    23:17

--- Tetris/source2.js
+++ Tetris/source2.js
@@ -3,8 +3,6 @@ let cfx = canvas.getContext('2d');
 
 let wall = Array(20).fill(0).map(()=>Array(10).fill(0));
 
-
-
 const renderWall = () => {
 	wall.map((a,i)=>{
 		a.map((b,j) => {
@@ -18,10 +16,33 @@ const renderWall = () => {
 	})
 }
 
+const canMove = () => {
+	return true;
+}
 
+const move = () = {
+	console.log("move");
+}
+
+let before = Date.now();
 const update = () => {
+	let current = Date.now();
+	if(current - before >= 350){
+		before = current;
+		canMove() && move();
+	}
 	renderWall();
 	requestAnimationFrame(update);
 }
 
 requestAnimationFrame(update);
+
+
+/*
+
+1. c언어 (마스터)하고 c++로 넘어가기 -> 하고싶은거
+2. c++하고 -> 하고싶은거
+3. 파이썬 -> 하고싶은거 강력해 쉬워
+
+
+*/
