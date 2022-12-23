ded9fd6ddc7fe072bdacba8d56f1d010ebc83035
중화이 <ljh15952@gmail.com>
2022-12-23 13:53:02 +0000

    tetris2 setting wall

--- Tetris/source2.js
+++ Tetris/source2.js
@@ -1,4 +1,27 @@
 let canvas = document.getElementById('canvas');
 let cfx = canvas.getContext('2d');
 
-cfx.fillRect(50,50,50,50);
+let wall = Array(20).fill(0).map(()=>Array(10).fill(0));
+
+
+
+const renderWall = () => {
+	wall.map((a,i)=>{
+		a.map((b,j) => {
+			if(b === 0){
+				cfx.fillStyle = 'gray';
+			}else if(b === 1){
+				cfx.fillStyle = 'black';
+			}
+			cfx.fillRect(j*11,i*11,10,10);
+		})
+	})
+}
+
+
+const update = () => {
+	renderWall();
+	requestAnimationFrame(update);
+}
+
+requestAnimationFrame(update);
